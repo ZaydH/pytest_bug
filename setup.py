25b17a2fe206803091d7e05e2e31e0c015e65c7d
Zayd Hammoudeh <zayd.hammoudeh@gmail.com>
2019-04-09 23:16:00 -0700

    Simple test case for pytest error.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="stratego",
+    version="0.0.0",
+    author="Zayd Hammoudeh",
+    packages=['stratego'],
+    # install_requires=['sty']
+)
---
+++ stratego/printer.py
@@ -0,0 +1,10 @@
+import sty
+from sty import fg, bg
+
+
+def add(x,y):
+    r"""
+    >>> add(5, 2)
+    7
+    """
+    return x + y
